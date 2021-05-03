63760aed508f8f114e36ebf4747f2e3d458b8822
PanduDLaksono <54766392+PanduDLaksono@users.noreply.github.com>
2021-05-03 14:30:13 +0700

    up praktikum modul 10

---
+++ src/10_firebase_reactjs/firebase/src/firebase.config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyAodw7Mn5FQuO5zTkSmgVIJs95yiheyZyk",
+    authDomain: "react-pbf.firebaseapp.com",
+    projectId: "react-pbf",
+    storageBucket: "react-pbf.appspot.com",
+    messagingSenderId: "389568950430",
+    appId: "1:389568950430:web:59766fb6f7aadb34184b8f",
+    measurementId: "G-3ESMNN4SDL"
+  };
